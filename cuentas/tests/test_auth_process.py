1114283d868ee80ea910cf52e1ed32ace6670262
reciproco <reciproco@gmail.com>
2016-07-03 19:04:02 +0200

    Added some cuentas tests

---
+++ cuentas/tests/test_auth_process.py
@@ -0,0 +1,59 @@
+from django.test import TestCase
+from django.test import Client
+from django.core.urlresolvers import reverse
+from cuentas.forms import RegistrationForm
+from cuentas.models import Profile
+import pdb
+
+
+class AuthTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+        self.form_data = {'username': 'prueba',
+                          'email': 'reciprocidad@gmail.com',
+                          'password1': 'peloto', 'password2': 'peloto'}
+
+    def test_registro_endpoint(self):
+        response = self.client.get(reverse('cuentas:registro'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_registro_valid_form(self):
+        # form_data = {'username': 'prueba', 'email': 'reciprocidad@gmail.com',
+        #             'password1': 'peloto', 'password2': 'peloto'}
+
+        form = RegistrationForm(data=self.form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_registro_invalid_email(self):
+        form_data = {'username': 'prueba', 'email': 'reciprocidad_gmail.com',
+                     'password1': 'peloto', 'password2': 'peloto'}
+
+        form = RegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_registro(self):
+        response = self.client.post(reverse('cuentas:registro'),
+                                    self.form_data)
+        self.assertRedirects(response, reverse('home'))
+
+    def test_login_without_activation(self):
+        self.test_registro()
+
+        form_data = {'username': 'prueba', 'password': 'peloto'}
+        response = self.client.post(reverse('cuentas:login'), form_data)
+        # self.assertEqual(response.status_code, 200)
+
+        # pdb.set_trace()
+        self.assertContains(response, 'active su cuenta usando')
+        # self.assertIn('_auth_user_id', self.client.session)
+
+    def test_login_with_activation(self):
+        self.test_registro()
+        p = Profile.objects.get(user_id=1)
+        response = self.client.get(
+                   reverse('cuentas:activar',kwargs={'key': p.activation_key}))
+        form_data = {'username': 'prueba', 'password': 'peloto'}
+        response = self.client.post(reverse('cuentas:login'), form_data)
+        # self.assertEqual(response.status_code, 200)
+        self.assertIn('_auth_user_id', self.client.session)
